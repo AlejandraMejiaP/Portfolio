3321c502aea37fd8121105b2bd2c38c871c243a2
AlejandraMejiaP <alejandra.mejia.3c@gmail.com>
2022-03-07 13:46:49 +0100

    publish github pages

--- src/components/Sections.js/About.js
+++ src/components/Sections.js/About.js
@@ -8,23 +8,22 @@ function About() {
           <i className="fa-solid fa-user-astronaut"></i>
         </div>
         <p className="about__txt--p">
-          Soy Alejandra Mejía Pérez, web developer. Mi relación con la
-          programación, aunque reciente, fue claramente amor a primera vista y,
-          cuando algo te apasiona, solo quieres seguir aprendiendo más y más.
+          ¡Hola mundo! Soy Alejandra Mejía, desarrolladora Web. Mi relación con
+          la programación, aunque reciente, fue claramente amor a primera vista
+          y, cuando algo te apasiona, solo quieres seguir aprendiendo más y más.
         </p>
 
         <p className="about__txt--p">
-          Todo empezó con el Bootcamp de Adalab, en noviembre del 2021. Allí
-          adquirí una valiosísima formación técnica (HTML, CSS, JavaScript,
-          React, Node JS, SQL, etc.) y un gran aprendizaje en habilidades tan
-          importantes como lo son el trabajo en equipo, la comunicación y el
-          trabajo en remoto, entre otras.
+          Antes de decidir reinventar mi trayectoria profesional yo era
+          historiadora. Al parecer, tuve que investigar los recovecos del pasado
+          antes de darme cuenta de que lo que realmente me interesaba era ¡el
+          futuro!
         </p>
         <p className="about__txt--p">
-          Soy una persona muy proactiva y tenaz en lo que me propongo. Mi
-          capacidad de aprendizaje y de enfrentarme a nuevos retos me ha sido de
-          especial utilidad a la hora de reinventar mi carrera profesional y
-          redirigirla al desarrollo web.
+          Soy una persona muy proactiva y tenaz en lo que me propongo. Me
+          encanta tener nuevos retos y desafios, algo que encuentro a diario en
+          la programación; siempre hay algo nuevo que aprender o mejorar. Los
+          retos son infinitos.
         </p>
         <p className="about__txt--p">
           Actualmente me encuentro muy entusiasmada por encontrar mi primera
